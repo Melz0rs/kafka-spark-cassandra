231b086f807119195fade8f7fe65d968f0bc24c6
Carmel Shupak <carmelshupak@gmail.com>
2019-06-08 16:29:08 +0300

    wip

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 from pyspark import SparkContext
+from operator import add
+
+sc = SparkContext(appName='spark_app')
+source_file = 'file:///Users/carmelshupak/Documents//kafka-spark-cassandra/log.txt'
+
+log4jLogger = sc._jvm.org.apache.log4j
+logger = log4jLogger.LogManager.getLogger(__name__)
+
+input = sc.textFile(source_file)
+
+logger.info("input: ", input.collect())
+
+# print(f"input: {input}")
+
