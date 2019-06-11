b241da97e3691f5842b7546c551c3cdf751a3641
Carmel Shupak <carmelshupak@gmail.com>
2019-06-11 10:20:07 +0300

    wip

--- app.py
+++ app.py
@@ -1,15 +1,41 @@
 from pyspark import SparkContext
 from operator import add
+import re
+
 
 sc = SparkContext(appName='spark_app')
-source_file = 'file:///Users/carmelshupak/Documents//kafka-spark-cassandra/log.txt'
+source_file = 'user_actions.txt'
 
 log4jLogger = sc._jvm.org.apache.log4j
 logger = log4jLogger.LogManager.getLogger(__name__)
 
-input = sc.textFile(source_file)
+input = sc.textFile('log.txt')
 
-logger.info("input: ", input.collect())
+# input = input.map(lambda x: x.split('\n'))\
+#              .map(lambda x: x[0].split('\t'))
+#              .map(lambda x: )
 
-# print(f"input: {input}")
+rdd = input.flatMap(lambda x: x.split('\t'))\
+              .filter(lambda x: x in ['facebook', 'twitter', 'bing'])\
+              .map(lambda x: (x, 1))\
+              .reduceByKey(add)
+
+
+a = rdd.collect()
+
+logger.info(a)
+
+
+
+# input = sc.textFile("yourfile.csv").map(lambda line: line.split(","))
+# df = sc.read.format("csv").option("header", "true").load(source_file)
+
+# input = input.map(lambda x: x['product'] = (x['product'], 1))  # .filter(lambda x: x.split('\n')).map(lambda x: x[0].split('\t'))
+
+# input = input.collect()
+
+# logger.info(input)
+
+
+
 
---
+++ red.py
@@ -0,0 +1,53 @@
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from operator import add
+
+
+sc = SparkContext(appName='spark_app')
+spark_sesiion = SparkSession(sc)
+source_file = 'user_actions.txt'
+
+log4jLogger = sc._jvm.org.apache.log4j
+logger = log4jLogger.LogManager.getLogger(__name__)
+
+input = sc.textFile('log.txt')
+
+# input = input.map(lambda x: x.split('\n'))\
+#              .map(lambda x: x[0].split('\t'))
+#              .map(lambda x: )
+
+rdd = input.flatMap(lambda x: x.split('\t'))\
+              .filter(lambda x: x in ['facebook', 'twitter', 'bing'])\
+              .map(lambda x: (x, 1))\
+              .reduceByKey(add)
+
+
+df = rdd.toDF()
+
+#
+sc._jsc.hadoopConfiguration().set("fs.s3.awsAccessKeyId", "AKIAIJOOCS3FE4QLGFKA")
+sc._jsc.hadoopConfiguration().set("fs.s3.awsSecretAccessKey", "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")
+
+# df = sc.read \
+#   .format("com.databricks.spark.redshift") \
+#   .option("url", "jdbc:redshift://redshifthost:5439/database?user=username&password=pass") \
+#   .option("dbtable", "my_table") \
+#   .option("tempdir", "s3n://path/for/temp/data") \
+#   .load()
+#
+# # Read data from a query
+# df = sc.read \
+#   .format("com.databricks.spark.redshift") \
+#   .option("url", "jdbc:redshift://redshifthost:5439/database?user=username&password=pass") \
+#   .option("query", "select x, count(*) my_table group by x") \
+#   .option("tempdir", "s3n://path/for/temp/data") \
+#   .load()
+
+# Write back to a table
+df.write \
+  .format("com.databricks.spark.redshift") \
+  .option("url", "jdbc:redshift://redshift-cluster-1.cipctqoqgqrk.eu-central-1.redshift.amazonaws.com:5439/dev?user=awsuser&password=Frnkvnkl12") \
+  .option("dbtable", "my_table_copy") \
+  .option("tempdir", "s3n://testcarmel/spark") \
+  .mode("error") \
+  .save()
